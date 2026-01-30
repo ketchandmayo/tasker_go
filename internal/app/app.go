84ad41dfaccb0202ccd4184807063b26fa928f40
Denis <83454515+ketchandmayo@users.noreply.github.com>
2026-01-30 16:13:32 +0500

    Starter improvements

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,28 +1,47 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"tasker_go/internal/analysis"
 	"tasker_go/internal/config"
+	"tasker_go/internal/llm/gemini"
 	"tasker_go/internal/repository/gorm"
 	"tasker_go/internal/service"
 	"tasker_go/internal/transport/http/handler"
 	"tasker_go/internal/transport/http/router"
 
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func Run() {
+	ctx := context.Background()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env не найден, используем системные переменные")
+	}
+
 	db := config.ConnectDB()
 	taskRepo := gorm.NewTaskRepository(db)
 	userRepo := gorm.NewUserRepository(db)
+	judgeRepo := gorm.NewJudgeRepository(db)
 
 	taskService := service.NewTaskService(taskRepo)
 	authService := service.NewAuthService(userRepo)
 
+	llm, err := gemini.NewClient(ctx, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	judgeAnalyzer := analysis.NewJudgeAnalyzer(llm)
+	judgeService := service.NewJudgeService(taskRepo, judgeRepo, judgeAnalyzer)
+
 	taskHandler := handler.NewTaskHandler(taskService)
 	authHandler := handler.NewAuthHandler(authService)
-	judgeHandler := handler.NewJudgeHandler()
+	judgeHandler := handler.NewJudgeHandler(judgeService)
 
 	r := router.New(taskHandler, authHandler, judgeHandler)
 
@@ -43,7 +62,7 @@ func Run() {
 		},
 	})
 
-	err := http.ListenAndServe(":8080", c.Handler(r))
+	err = http.ListenAndServe(":8080", c.Handler(r))
 	if err != nil {
 		log.Printf("Server error")
 	}
